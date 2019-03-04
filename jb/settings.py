c7e3547c4301d19371aae4d94e6881fe3a63ed75
Yuvaraju Meenuga <yuvaraj.iitkgp@gmail.com>
2019-03-04 16:12:26 +0530

    APIs for list of tables, columns for a given source

--- jb/settings.py
+++ jb/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders',
     'jb_dashboard',
     'jb_settings'
 ]
@@ -51,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
 ]
 

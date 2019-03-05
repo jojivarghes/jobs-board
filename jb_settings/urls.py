3ef7737af693fee5d1a5f54080a72263b94bcf8d
Yuvaraju Meenuga <yuvaraj.iitkgp@gmail.com>
2019-03-05 13:17:45 +0530

    Allow backend CORS support; Connect to MySQL databases

--- jb_settings/urls.py
+++ jb_settings/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view()),
+    path('test/', views.mysql_testing),
     path('<int:id>/', views.SourcesView.as_view()),
     path('<int:id>/tables', views.list_of_tables),
     path('<int:id>/tables/<str:tableName>/columns', views.columns_for_table),
